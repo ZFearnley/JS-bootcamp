5b6c85e72b6bf48071df3dbfa3c8480c89bef6fb
ZFearnley <ZFearnley213@gmail.com>
2019-08-07 10:54:27 +1200

    new update

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+let name = "Nelson"
+name = "NZ"
+name = name + " Zane"
+console.log(name)
+
+const lastName = "Fearnley"
+const firstName = "Zane"
+console.log(lastName)
+console.log(firstName)
+
+const numOne = 100.99
+const numTwo = 56.97
+
+const result = numOne - numTwo
+console.log(result)
